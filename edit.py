c8c89e0deeb4b7a9fa367144246625153cf13b96
Doctor-Ink <bezusov.88@mail.ru>
2022-12-21 23:41:22 +0300

    ADD: functionalition delete extra spaces

--- edit.py
+++ edit.py
@@ -1,8 +1,16 @@
 
-
+import re
 
 with open('text1.txt', mode='r', encoding='utf-8') as file:
-    all_text = file.read()
-    # print(all_text)
-    for line in all_text.split('\n'):
-        print(line)
+    new_str = ''
+    for line in file.readlines():
+        if len(line) > 1 and line[-2] in '.!?...' and line[-1] == '\n':
+            new_str += line
+        else:
+            new_str += line[:-1:]
+    print(new_str)
+
+
+my_str = "12. Определить       количество теплоты, переданного   в процессе теплового из"
+print(my_str.replace(('  ', '   '), ' '))
+
--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 # /usr/bin/env python 3.10
-
 import pytesseract
 from PIL import Image
+import os
 
-# указывам фото, которое необходимо распознать
-img = Image.open(r'C:\Users\Zver\PycharmProjects\photo_text_recognising\input_photos\термодинамика и теплотехника.jpg')
 # указываем путь к исполняемому файлу программы тессеракт
 pytesseract.pytesseract.tesseract_cmd = r'c:\Users\Zver\AppData\Local\Tesseract-OCR\tesseract.exe'
 
 # настройка точности распознования
 custom_config = r'--oem 3 --psm 6'
 
-text = pytesseract.image_to_string(img, lang='rus', config=custom_config)
-print(text)
-with open(r'output_text\text.txt', mode='w', encoding='utf-8') as file:
-    file.write(text)
+
+def recognise():
+    ''' функция распознаёт все фото в указанной папке input_photos'''
+
+    list_photos = os.listdir('input_photos')
+    count = 1
+    for item in list_photos:
+        img = Image.open(f'input_photos\\{item}')
+        text = pytesseract.image_to_string(img, lang='rus', config=custom_config)
+        with open(r'output_text\text.txt', mode='a', encoding='utf-8') as file:
+            file.write(text)
+        print(f"[INFO] {count}/{len(list_photos)} ")
+        count +=1
 
 
+def extra_spaces():
+    ''' функция убирает лишние пробелы'''
+
+    with open(r'output_text\text.txt', mode='r', encoding='utf-8') as file:
+        reader = file.read()
+        reader.replace('  ', ' ')
+        print(reader)
+
+
+def main():
+    recognise()
+    extra_spaces()
+
+
+if __name__=='__main__':
+    main()
 
